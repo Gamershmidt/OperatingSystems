8cb58ddb84c64af322671290e432d011445528bd
Gamershmidt <68111336+Gamershmidt@users.noreply.github.com>
2023-09-11 21:53:36 +0300

    Delete tree/master/week01 directory

--- tree/master/week01/ex1.sh
+++
@@ -1,7 +0,0 @@
-# Creation of a directory
-mkdir -p /home/sofia/OS
-mkdir -p /home/sofia/OS/week01
-cd /home/sofia/OS/week01
-
-# Exercise 1
-ls -1 /usr/bin | grep gcc | sort -r | tail -n 5 > ex1.txt
--- tree/master/week01/ex2.sh
+++
@@ -1,7 +0,0 @@
-cd /home/sofia/OS/week01
-ls -1
-ls -1 > test_file_forex2.txt
-cat test_file_forex2.txt
-
-#Saving command history to txt
-history 5 > ex2.txt
