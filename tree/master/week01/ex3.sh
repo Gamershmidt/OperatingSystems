5805404970a9f822479feb1ebbc31b107da1a2b1
Gamershmidt <68111336+Gamershmidt@users.noreply.github.com>
2023-09-11 21:14:38 +0300

    Add files via upload

---
+++ tree/master/week01/ex3.sh
@@ -0,0 +1,14 @@
+sleep 3 | date
+mkdir /home/sofia/OS/week01/root_folder
+sleep 3 | date
+cd /
+ls -ltr > /home/sofia/OS/week01/root_folder/root.txt
+sleep 3 | date
+mkdir /home/sofia/OS/week01/home_folder
+sleep 3 | date
+cd ~
+ls -ltr > /home/sofia/OS/week01/home_folder/home.txt
+ls /home/sofia/OS/week01/home_folder
+cat /home/sofia/OS/week01/home_folder/home.txt
+ls /home/sofia/OS/week01/root_folder
+cat /home/sofia/OS/week01/root_folder/root.txt
