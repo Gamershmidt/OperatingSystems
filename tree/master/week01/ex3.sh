0f77c35a5061a52bfca74872d6d876f547e6ae5a
Gamershmidt <68111336+Gamershmidt@users.noreply.github.com>
2023-09-11 21:24:49 +0300

    Make it readable

--- tree/master/week01/ex3.sh
+++ tree/master/week01/ex3.sh
@@ -1,13 +1,22 @@
+#Creating folder for root.txt
 sleep 3 | date
 mkdir /home/sofia/OS/week01/root_folder
-sleep 3 | date
+
+#Write list of files of / into root.txt
 cd /
+sleep 3 | date
 ls -ltr > /home/sofia/OS/week01/root_folder/root.txt
+
+#Creating folder for home.txt
 sleep 3 | date
 mkdir /home/sofia/OS/week01/home_folder
-sleep 3 | date
+
+#Write list of files of / into root.txt
 cd ~
+sleep 3 | date
 ls -ltr > /home/sofia/OS/week01/home_folder/home.txt
+
+#Display content of files and folders
 ls /home/sofia/OS/week01/home_folder
 cat /home/sofia/OS/week01/home_folder/home.txt
 ls /home/sofia/OS/week01/root_folder
