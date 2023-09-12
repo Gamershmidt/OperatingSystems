e2561753ec48a7147556b221c2bff5a51f213388
Gamershmidt <68111336+Gamershmidt@users.noreply.github.com>
2023-09-12 12:33:00 +0300

    Proper paths

--- week01/ex1.sh
+++ week01/ex1.sh
@@ -1,7 +1,6 @@
 # Creation of a directory
-mkdir -p /home/sofia/OS
-mkdir -p /home/sofia/OS/week01
-cd /home/sofia/OS/week01
+mkdir ~/week01
+cd ~/week01
 
 # Exercise 1
 ls -1 /usr/bin | grep gcc | sort -r | tail -n 5 > ex1.txt
