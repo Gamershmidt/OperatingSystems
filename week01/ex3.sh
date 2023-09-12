ac5262c76360a66851cf41fe515a00589513efbd
Gamershmidt <68111336+Gamershmidt@users.noreply.github.com>
2023-09-12 12:36:44 +0300

    Update ex3.sh

--- week01/ex3.sh
+++ week01/ex3.sh
@@ -1,23 +1,23 @@
 #Creating folder for root.txt
 sleep 3 | date
-mkdir /home/sofia/OS/week01/root_folder
+mkdir ~/week01/root_folder
 
 #Write list of files of / into root.txt
 cd /
 sleep 3 | date
-ls -ltr > /home/sofia/OS/week01/root_folder/root.txt
+ls -ltr > ~/week01/root_folder/root.txt
 
 #Creating folder for home.txt
 sleep 3 | date
-mkdir /home/sofia/OS/week01/home_folder
+mkdir ~/week01/home_folder
 
 #Write list of files of ~ into home.txt
 cd ~
 sleep 3 | date
-ls -ltr > /home/sofia/OS/week01/home_folder/home.txt
+ls -ltr > ~/week01/home_folder/home.txt
 
 #Display content of files and folders
-ls /home/sofia/OS/week01/home_folder
-cat /home/sofia/OS/week01/home_folder/home.txt
-ls /home/sofia/OS/week01/root_folder
-cat /home/sofia/OS/week01/root_folder/root.txt
+ls ~/week01/home_folder
+cat ~/week01/home_folder/home.txt
+ls ~/week01/root_folder
+cat ~/week01/root_folder/root.txt
