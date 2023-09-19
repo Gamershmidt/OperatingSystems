2c65c2a5c00ff01300ae3d519cba9c65897dac2e
Gamershmidt <68111336+Gamershmidt@users.noreply.github.com>
2023-09-19 20:40:10 +0300

    Update ex1.c

--- week02/ex1.c
+++ week02/ex1.c
@@ -3,6 +3,7 @@
 #include <limits.h>
 int main()  {
 	int x;
+	unsigned short int USI = USHRT_MAX;
 	signed long int SLI;
 	float fl;
 	double dl;
@@ -11,6 +12,7 @@ int main()  {
 	dl = DBL_MAX;
 	fl = FLT_MAX;
 	printf("Integer size: %ld, value: %d \n", sizeof(x), x);
+	printf("Unsigned short integer size: %ld, value: %hu \n", sizeof(USI), USI);
 	printf("Signed long integer size: %ld, value: %ld \n", sizeof(SLI), SLI);
 	printf("Float size: %ld, value: %f \n", sizeof(fl), fl);
 	printf("Double size: %ld, value: %lf \n", sizeof(dl), dl);
