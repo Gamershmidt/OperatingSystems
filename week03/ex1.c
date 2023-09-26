dfa66ef52f69d4f9a5a5658da5478bbb2f558373
Gamershmidt <68111336+Gamershmidt@users.noreply.github.com>
2023-09-26 22:41:45 +0300

    Create ex1.c

---
+++ week03/ex1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int const_tri(int* p, int n) {
+    if(n < 3) return *p;
+    if(n == 3) return *(p + 2);
+    else return const_tri(p, n - 1) + const_tri(p, n - 2) + const_tri(p, n - 3);
+}
+int main() {
+    const int x = 1;
+    const int* q = &x;
+    int* p = (int*)calloc(3, sizeof(int));
+    
+    
+    *p = *q;
+    *(p + 1) = *q;
+    *(p + 2) = 2 * (*q);
+
+    fprintf(stdout, "%p %p %p\n", p, (p + 1), (p + 2));
+    fprintf(stdout, "%d", const_tri(p, 6));
+
+    free(p); // Don't forget to free the allocated memory when done
+
+    return 0;
+}
