d6b8e4921042d0d69df770ca6402e78a9d980220
Gamershmidt <68111336+Gamershmidt@users.noreply.github.com>
2023-10-03 17:38:52 +0300

    Update ex1.c

--- week04/ex1.c
+++ week04/ex1.c
@@ -5,9 +5,9 @@
 int main()
 {
 	pid_t child1, child2;
-	clock_t begin = clock();
-	child1 = fork();
 	
+	child1 = fork();
+	clock_t begin = clock();
 	if (child1 == 0) {
 	    clock_t begin = clock();
 	    printf("Child-process ID: %d, Parent ID: %d\n", getpid(), getppid());
