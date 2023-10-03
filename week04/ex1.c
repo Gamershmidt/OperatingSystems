1a976df8285d73c827fe427ccbb0ffe94bd28e5d
Gamershmidt <68111336+Gamershmidt@users.noreply.github.com>
2023-10-03 19:32:58 +0300

    Update ex1.c

--- week04/ex1.c
+++ week04/ex1.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <time.h>
+#include <stdlib.h>
 int main()
 {
 	pid_t child1, child2;
-	
 	child1 = fork();
-	clock_t begin = clock();
+	clock_t begin1 = clock();
 	if (child1 == 0) {
-	    clock_t begin = clock();
 	    printf("Child-process ID: %d, Parent ID: %d\n", getpid(), getppid());
-	
-	    printf("Time: %ld\n",clock() - begin);
+	    clock_t end1 = clock();
+	    printf("Time: %ld\n",end1 - begin1);
+	    exit(0);
 	} else {
 	    child2 = fork();
-	  
-	
-	    if (child2 == 0) {
-	        clock_t begin = clock();
-		printf("Child-process ID: %d, Parent ID: %d\n", getpid(), getppid());
-	        printf("Time: %ld\n", clock() - begin);
-	    } else {
-		printf("Parent-process ID: %d, Parent ID: %d\n", getpid(), getppid());
+	    clock_t begin2 = clock();
 
-	        printf("Time: %ld\n", clock() - begin);
+	    if (child2 == 0) {
+		printf("Child-process ID: %d, Parent ID: %d\n", getpid(), getppid());
+		clock_t end2 = clock();
+	        printf("Time: %ld\n", end2 - begin2);
+	        exit(0);
+	    } else {
+	    	wait(NULL);
+	    	wait(NULL);
+		printf("Parent-process ID: %d, Parent ID: %d\n", getpid(), getppid());
+		clock_t end1 = clock();
+	        printf("Time: %ld\n", end1 - begin1);
 	    }
 	}
 	return 0;
